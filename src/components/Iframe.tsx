4b475acb5a3c782297be048467e900ba491f6bcd
Suraj <surajgupta.college@gmail.com>
2023-11-23 22:12:39 +0530

    updated iframe

--- src/components/Iframe.tsx
+++ src/components/Iframe.tsx
@@ -1,6 +1,6 @@
 // origial code: https://gist.github.com/threepointone/e73a87f7bbbebc78cf71744469ec5a15
 
-import { Suspense, useLayoutEffect, useRef } from 'react';
+import { Suspense, useLayoutEffect, useRef, useState } from 'react';
 import IframeLoader from '@/components/IframeLoader';
 
 type IFrameProps = React.ComponentPropsWithRef<'iframe'> & {
@@ -22,20 +22,26 @@ function IFrameImplementation(props: React.ComponentPropsWithRef<'iframe'>) {
     promise: null | Promise<void>;
     resolve: () => void;
     reject: () => void;
-  }>(null);
-  if (awaiter.current?.promise) {
+}>(null);
+
+/* eslint-disable @typescript-eslint/no-unused-vars  */
+const [_, triggerLoad] = useState(false)
+
+if (awaiter.current?.promise) {
     throw awaiter.current.promise;
-  }
-  useLayoutEffect(() => {
+}
+
+useLayoutEffect(() => {
     if (awaiter.current === null) {
-      // @ts-expect-error: no need for null check
-      awaiter.current = {};
-      // @ts-expect-error: no need for null check
-      awaiter.current.promise = new Promise<void>((resolve, reject) => {
-        if (awaiter.current) {
-          Object.assign(awaiter.current, { resolve, reject });
-        }
-      });
+        // @ts-expect-error: no need for null check
+        awaiter.current = {}
+        // @ts-expect-error: no need for null check
+        awaiter.current.promise = new Promise<void>((resolve, reject) => {
+            if (awaiter.current) {
+                Object.assign(awaiter.current, { resolve, reject });
+            }
+        });
+        triggerLoad(true)
     }
   }, []);
   const { title } = props;
