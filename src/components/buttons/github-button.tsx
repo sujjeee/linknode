dee0124aea4f75a2433862952360e695ed76d5b2
Suraj <surajgupta.college@gmail.com>
2023-11-27 14:26:47 +0530

    fixed linting

--- src/components/buttons/github-button.tsx
+++ src/components/buttons/github-button.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
 import React from 'react';
-import { buttonVariants } from '../ui/button';
+import Link from 'next/link';
+import { buttonVariants } from '@/components/ui/button';
 import { Github } from 'lucide-react';
 
 export default function GithubButton() {
